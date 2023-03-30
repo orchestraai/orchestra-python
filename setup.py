ea6da95bc9d1d3cc58d92a3b4df87abc8c02a733
Jason Jin <jasonjinhuan@gmail.com>
2023-03-30 00:57:45 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='orchestra',
+    version='0.0.1',
+    license='MIT',
+    author="Jason Jin",
+    author_email='jason@useorchestra.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/orchestraai/orchestra-sdk',
+    keywords='orchestra python',
+    install_requires=[
+          'open-ai'
+    ],
+)
